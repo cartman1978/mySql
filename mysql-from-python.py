3f83546706344b306332632445d327cab64861e5
cartman1978 <cartman_7802@yahoo.ie>
2021-05-28 19:34:55 +0000

    Table Friends created

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+# import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -14,10 +15,10 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        cursor.execute("""CREATE TABLE IF NOT EXISTS
+                        Friends(name char(20), age int, DOB datetime);""")
+        for row in cursor:
+            print(row)
 finally:
     # Close the connection, regardless of whether or not the above was success
     connection.close() 
