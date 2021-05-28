6687f0b753914901a41ba371fa0882d6fd2e5864
cartman1978 <cartman_7802@yahoo.ie>
2021-05-28 19:57:49 +0000

    Added many Tables

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,5 +1,5 @@
 import os
-# import datetime
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -15,10 +15,11 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS
-                        Friends(name char(20), age int, DOB datetime);""")
-        for row in cursor:
-            print(row)
+        rows = [("Bob", 21, "1990-02-06 23:04:56"),
+               ("Jim", 56, "1956-05-09 13:12:45"),
+               ("Nello", 51, "1961-04-25 18:06:35")]
+        cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", rows)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether or not the above was success
     connection.close() 
